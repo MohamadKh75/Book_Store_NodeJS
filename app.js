cc026cf19bf3468f4aad717b5ccd7e8b45315b9b
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-08-25 16:28:23 +0430

    Connect to Mongoose

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ var mongoose = require ('mongoose');
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
 
+app.get('/', function(req, res)
+    {
+        res.send('Hello World!');
+    });
+
+app.listen(3000);
+console.log('Running on port 3000!');
